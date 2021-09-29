ae43ab3e5d6c9598cdaf0b153dd1ef75dd6fd847
DALGA Ozguc <ozgucdalga@gmail.com>
2021-09-29 13:58:23 -0400

    props drilling

--- books/src/components/BookList.js
+++ books/src/components/BookList.js
@@ -1,21 +1,41 @@
-import React  from 'react';
+import React from 'react';
 import Book from './Book';
+import './BookList.css'
 
 class BookList extends React.Component {
 
 
 
-    render () {
+    render() {
         console.log(this.props.books)
 
         const bookList = this.props.books.map((book, i) => {
-            return < Book  book={book}
+            return < Book book={book}
                 key={i}
-                 />
+            />
         })
-        return <div> {bookList} </div>
+        return (
+
+            <section className="page-section bg-light" id="portfolio">
+                <div className="container">
+                    <div className="text-center">
+                        <h2 className="section-heading text-uppercase">BOOKFOLIO</h2>
+                        <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    </div>
+                    <div className="row">
+                        {bookList}
+                    </div>
+                </div>
+            </section>
+
+
+        );
     }
-    
+
 }
 
 export default BookList;
+
+
+
+
